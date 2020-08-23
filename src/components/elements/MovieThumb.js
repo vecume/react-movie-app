3540d29f3bf5149afc0fa802c18df1e2c774ad5e
vecume <shahzod.bek200109@gmail.com>
2020-08-23 20:30:41 +0500

    Fix problem with routing

--- src/components/elements/MovieThumb.js
+++ src/components/elements/MovieThumb.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import { Link } from '@reach/router';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Link } from "@reach/router";
+import PropTypes from "prop-types";
 
 //Styles
-import { StyledMovieThumb } from '../styles/StyledMovieThumb';
+import { StyledMovieThumb } from "../styles/StyledMovieThumb";
 /////////////////
 
 function MovieThumb({ image, movieId, clickable, movieName }) {
-
   return (
-    <StyledMovieThumb className={clickable && 'clickable-thumb'}>
+    <StyledMovieThumb className={clickable && "clickable-thumb"}>
       {clickable ? (
-        <Link to={`movie/${movieId}`} title={movieName}>
+        <Link to={`/movie/${movieId}`} title={movieName}>
           <img src={image} alt={movieName} />
           <h4>{movieName}</h4>
         </Link>
       ) : (
-          <img src={image} alt={movieName} />
-        )}
+        <img src={image} alt={movieName} />
+      )}
     </StyledMovieThumb>
-  )
+  );
 }
 
 export default MovieThumb;
@@ -28,5 +27,5 @@ MovieThumb.propTypes = {
   image: PropTypes.string,
   movieId: PropTypes.number,
   clickable: PropTypes.bool,
-  movieName: PropTypes.string
-}
+  movieName: PropTypes.string,
+};
