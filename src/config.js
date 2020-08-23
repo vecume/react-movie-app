7584f11268e89fcd0bea00af09f474c3a62b6b17
vecume <shahzod.bek200109@gmail.com>
2020-08-23 20:35:07 +0500

    Add https

--- src/config.js
+++ src/config.js
@@ -2,17 +2,24 @@
 // To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=cc390de7fba1dc68ee6c3e5d73c22371
 // Read more about the API here: https://developers.themoviedb.org/
 
-const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = 'cc390de7fba1dc68ee6c3e5d73c22371';
-const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+const API_URL = "https://api.themoviedb.org/3/";
+const API_KEY = "cc390de7fba1dc68ee6c3e5d73c22371";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
 
 const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&query=`;
 const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}`;
 
-
 // Sizes: w300, w780, w1280, original
-const BACKDROP_SIZE = 'w1280';
+const BACKDROP_SIZE = "w1280";
 // w92, w154, w185, w342, w500, w780, original
-const POSTER_SIZE = 'w500';
+const POSTER_SIZE = "w500";
 
-export { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE, SEARCH_BASE_URL, POPULAR_BASE_URL };
+export {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  BACKDROP_SIZE,
+  POSTER_SIZE,
+  SEARCH_BASE_URL,
+  POPULAR_BASE_URL,
+};
